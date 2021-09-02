eea5e7bbca6bf80e74778b7d0b96016c817f2a4c
chandralekhamaity <88969019+chandralekhamaity@users.noreply.github.com>
2021-09-02 19:07:45 +0530

    Add files via upload

---
+++ python variables.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 17 12:11:28 2021
+
+@author: CHANDRALEKHA
+"""
+
+V = [1,2,3,4,5,6,7]
+print(V)
+
+import matplotlib.pyplot as plt
+plt.plot(V)
+
+
+
+list1 = [1 , "hello" , "world" , True]
+
+list2 = list(range(10))
+
+print(list1)
+
+print(list2)
+
+print(list1[0])
+
+print(list1[1:3])
+
+print(list1[-3])
+
+print(list1[2])
