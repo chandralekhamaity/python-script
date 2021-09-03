25fe10cf1e80bb5115b1e80048411cf771360629
chandralekhamaity <88969019+chandralekhamaity@users.noreply.github.com>
2021-09-03 09:50:08 +0530

    Add files via upload

---
+++ learning spyder.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 14 09:17:14 2021
+
+@author: CHANDRALEKHA
+"""
+x = 5
+
+y = 7
+
+z1 = x + y
+
+z2 = x - y
+
+z3 = x*y
+
+z4 = x/y
+
+z5 = x + 2*y
+
+z6 = 3*x + 2*y
+
+z7 = x**2*y**2
